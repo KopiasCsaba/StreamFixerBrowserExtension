17509cd33d94dfc1bae03e2ab4c0976f48a9bd26
Kopiás Csaba <metamorfozis@gmail.com>
2021-01-15 11:55:21 +0100

    Update build tools

--- tools/chromecompile.sh
+++ tools/chromecompile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$DIR"
-
+echo "<CHROME COMPILE> ######################"
 bash ./copy.sh
 cd ../chrome/;
 zip -r -FS ../builds/StreamFixerChrome.zip ./* --exclude *.git*.
+echo "</CHROME COMPILE> ######################"
--- tools/firefoxcompile.sh
+++ tools/firefoxcompile.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 cd "$DIR"
-
+echo "<FIREFOX COMPILE> ######################"
 bash ./copy.sh
 cd ../firefox/;
 zip -r -FS ../builds/StreamFixerFirefox.xpi  ./* --exclude *.git*.
+echo "</FIREFOX COMPILE> ######################"
